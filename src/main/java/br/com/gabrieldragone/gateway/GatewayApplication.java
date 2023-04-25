78e37d5911b3d1fe7a44a4c6808594662245ab6a
Gabriel Alves <37886654+GabrielDragone@users.noreply.github.com>
2023-04-24 22:07:34 -0300

    Configuração do API Gateway para se registrar no Eureka Server e realizar o roteamento e load balancer

--- src/main/java/br/com/gabrieldragone/gateway/GatewayApplication.java
+++ src/main/java/br/com/gabrieldragone/gateway/GatewayApplication.java
@@ -2,8 +2,10 @@ package br.com.gabrieldragone.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Habilita para que a aplicação se registre no Eureka Server
 public class GatewayApplication {
 
 	public static void main(String[] args) {
